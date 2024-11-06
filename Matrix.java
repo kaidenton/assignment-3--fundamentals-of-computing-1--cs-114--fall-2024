31b9d2f3596a553ce615bcc27a340d2852366dff
kaidenton <notnediak@gmail.com>
2024-11-06 16:59:22 -0500

    added code that will print the default numbers and added code that will print the table of numbers

--- Matrix.java
+++ Matrix.java
@@ -1,13 +1,48 @@
 public class Matrix {
-    private static void printMatrix(int[][] table) {
-        
+    void printMatrix(int[][] table) {
+        final String ANSI_RESET = "\u001B[37m";
+        final String ANSI_YELLOW = "\u001B[33m";
+
+        for(int row = 0; row < table.length; row++) {
+            for(int col = 0; col < table.length; col++) {
+              //There is a minus 1 because they start at 0, meaning col would never equal 4
+              if(col + row == table.length - 1){
+                System.out.print(ANSI_YELLOW + "\t" + table[row][col] + ANSI_RESET);
+              }else{
+                System.out.print("\t" + table[row][col]);
+              }
+            }
+            System.out.println(" ");
+        }
     }
 
-    private static void populateMatrix(int[][] table) {
-    
+    void populateMatrix(int[][] table) {
+        int value = 1;
+        //ANSI_RESET will make the font color white
+        final String ANSI_RESET = "\u001B[37m";
+        //ANSI_YELLOW will make the font color yellow
+        final String ANSI_YELLOW = "\u001B[33m";
+        //Gives all the numbers their corresponding numbers
+        for(int row = 0; row < table.length; row++) {
+            for(int col = 0; col < table.length; col++) {
+                table[row][col] = value++;
+            }
+        }
+
+        for(int row = 0; row < table.length; row++) {
+            for(int col = 0; col < table.length; col++) {
+              //There is a minus 1 because they start at 0, meaning col would never equal 4
+              if(col + row == table.length - 1){
+                System.out.print(ANSI_YELLOW + "\t" + table[row][col] + ANSI_RESET);
+              }else{
+                System.out.print("\t" + table[row][col]);
+              }
+            }
+            System.out.println(" ");
+        }
     }
-    
-    private static void flipMatrix(int[][] table) {
+
+    void flipMatrix(int[][] table) {
     
     }
 }
