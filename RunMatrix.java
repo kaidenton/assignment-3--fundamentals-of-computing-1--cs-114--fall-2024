0106c2731a495c463f32c7272ee0b41e91e131ff
kaidenton <notnediak@gmail.com>
2024-11-06 16:07:15 -0500

    added user input and prints how big the matrix will be

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int userInput;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("\tPlease enter the size of your matrix: ");
+    userInput = input.nextInt();
+    input.close();
+
+    System.out.println(" ");
+    System.out.println("\tYour matrix is " + userInput + " x " + userInput);
   }
 }
